52db78db5ada3f1aad62b6fe2793f277ea8a2c95
tilenmiklavic <tilen.miklavic@celtra.com>
2026-01-03 23:17:04 +0100

    check clearance

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 from tkinter import *
-import random
 from PIL import Image, ImageDraw
+import random
+import math
 
 
 root = Tk()
-cHeight = 595
 
+cHeight = 595
 cWidith = 842
 numPoints = 50 #input("Število točk: ")
+POINT_CLEARANCE = 15
+
+points = []
 
 image = Image.new(mode="RGB", size=(cWidith, cHeight), color="white")
 draw = ImageDraw.Draw(image)
 
+def isInRadius(point1, point2, radius):
+    return math.sqrt((abs(point1[0] - point2[0]) ** 2 + abs(point1[1] - point2[1]) ** 2)) < radius
+
+def doesOverlap(point, points, distance):
+    return any(isInRadius(point, p, distance) for p in points)
+
 def drawPoints(numPoints):
 
-    for i in range(numPoints):
+    while len(points) < numPoints:
         x = random.randrange(10, cWidith-10)
         y = random.randrange(10, cHeight-10)
+        potentialPoint = (x, y)
+
+        if doesOverlap(potentialPoint, points, POINT_CLEARANCE):
+            continue
+
+        points.append(potentialPoint)
+
         draw.ellipse((x, y, x + 3, y + 3) ,fill="black", width=3)
-        draw.text((x-3, y-11), text=(str(i+1)), fill="black", size=10)
+        draw.text((x-3, y-11), text=(str(len(points))), fill="black", size=10)
     return
 
 drawPoints(int(numPoints))
